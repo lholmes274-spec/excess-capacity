9c617b51921e0bb77dd367c5b92558052856e11e
Lamar Holmes <238115297+lholmes274-spec@users.noreply.github.com.>
2026-03-03 11:32:56 -0600

    Installed Google Tag Manager (GTM)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -48,33 +48,36 @@ export default function RootLayout({
         {/* Facebook App ID */}
         <meta property="fb:app_id" content="2963411410513274" />
 
-        {/* ✅ Google Tag (Ads + GA4 shared loader) */}
-        <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=AW-17728116849"
-        ></script>
-
+        {/* Google Tag Manager */}
         <script
           dangerouslySetInnerHTML={{
             __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-
-              // Google Ads
-              gtag('config', 'AW-17728116849');
-
-              // Google Analytics 4
-              gtag('config', 'G-9BJEGLTDSK', {
-                page_path: window.location.pathname,
-              });
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;
+              f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-XXXXXXX');
             `,
           }}
         />
+        {/* End Google Tag Manager */}
       </head>
 
-      <body className={`${inter.className} bg-gray-50 text-gray-900 overflow-x-hidden`}>
-        {/* Wrap all pages in ClientLayout */}
+      <body
+        className={`${inter.className} bg-gray-50 text-gray-900 overflow-x-hidden`}
+      >
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-XXXXXXX"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+
         <ClientLayout>{children}</ClientLayout>
       </body>
     </html>
