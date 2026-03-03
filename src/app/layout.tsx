c094e6f2c4f64e79ec8584a1b7224f41c0ba8b75
Lamar Holmes <238115297+lholmes274-spec@users.noreply.github.com.>
2026-03-03 11:47:00 -0600

    Added GTM alongside existing gtag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -48,6 +48,21 @@ export default function RootLayout({
         {/* Facebook App ID */}
         <meta property="fb:app_id" content="2963411410513274" />
 
+        {/* 🔥 Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;
+              f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-P7HCLHN5');
+            `,
+          }}
+        />
+        {/* End Google Tag Manager */}
+
         {/* ✅ Google Tag (Ads + GA4 shared loader) */}
         <script
           async
@@ -74,7 +89,18 @@ export default function RootLayout({
       </head>
 
       <body className={`${inter.className} bg-gray-50 text-gray-900 overflow-x-hidden`}>
-        {/* Wrap all pages in ClientLayout */}
+        
+        {/* 🔥 Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-P7HCLHN5"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+
         <ClientLayout>{children}</ClientLayout>
       </body>
     </html>
